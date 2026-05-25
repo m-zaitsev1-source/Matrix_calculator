fac150c46ff5c5a6ec0b42ee79108d5bc8407d8f
m-zaitsev1-source <misharik15031@gmail.com>
2026-05-25 04:27:27 +0000

    да вообще пофиг

--- test_matrix.py
+++ test_matrix.py
@@ -4,6 +4,6 @@ def print_matrix(mat):
     for i in range(mat.row):
         print(mat.data[i])
 a1 = matrix.create(3, 3)
-a1.data = [[0, 0, 1], [5,4,3], [2, 1, 0]]
+a1.data = [[1, 1/2, 1/3], [1/2,1/3,1/4], [1/3,1/4,1/5]]
 print(matrix.is_valid(a1))
 print(matrix.determinant(a1))
